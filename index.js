828e1c4278b7919659c2d1be1c1d9377d9c795ac
Jason Hernandez <kaltre10@gmail.com>
2022-06-14 12:45:57 -0500

    no path

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const routerApi = require('./network/routerApi');
-const path = require('path');
 const cors = require('cors')
 const app = express();
 app.use(cors());
@@ -10,8 +9,6 @@ app.use(express.json());
 connection();
 
 routerApi(app);
-
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, './dist/index.html')));
 
 const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, () => console.log(`server en el puerto ${PORT}`))
